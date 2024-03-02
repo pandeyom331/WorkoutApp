0c1a3f3771a2205d9f0b8e37373ae6915bee7011
pandeyom331 <pandeyom331@gmail.com>
2024-03-02 16:14:32 +0530

    setup expo router

--- src/components/ExerciseListItem.jsx
+++ src/components/ExerciseListItem.jsx
@@ -1,31 +1,42 @@
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function ExerciseListItem({item}){
+export default function ExerciseListItem({ item }) {
     return (
-      <View style={styles.exerciseContainer}>
-        <Text style={styles.exerciseName}>
-          {item.name}
-        </Text>
-        <Text style={styles.exerciseSubtitle}>
-          {item.muscle.toUpperCase()} | {item.equipment.toUpperCase()}
-        </Text>
-      </View>
+        <View style={styles.exerciseContainer}>
+            <Text style={styles.exerciseName}>
+                {item.name}
+            </Text>
+            <Text style={styles.exerciseSubtitle}>
+                {item.muscle.toUpperCase()} | {item.equipment.toUpperCase()}
+            </Text>
+        </View>
     );
-  }
+}
 
 
-  const styles = StyleSheet.create({
+const styles = StyleSheet.create({
     exerciseContainer: {
-      backgroundColor: '#fff',
-      padding: 10,
-      borderRadius: 10,
-      gap: 5
+        backgroundColor: '#fff',
+        padding: 10,
+        borderRadius: 10,
+        gap: 10,
+        marginHorizontal:2,
+
+        shadowColor: "#000000",
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 1.51,
+        elevation: 2,
+
     },
     exerciseName: {
-      fontSize: 20,
-      fontWeight: '500',
+        fontSize: 20,
+        fontWeight: '500',
     },
     exerciseSubtitle: {
-      color: 'dimgray'
+        color: 'dimgray'
     }
-  });
+});
